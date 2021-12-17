7df8a5e0498fb6bbd61a032a1b94a3ad1c521614
purbanus <purbanus@gmail.com>
2021-12-17 17:10:21 +0100

    Added Pging and Sorting (#1)

--- Spring-Data-JPA-Tutorial-Following-2/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ Spring-Data-JPA-Tutorial-Following-2/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.CourseMaterial;
@@ -15,9 +19,9 @@ import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Teacher;
 @SpringBootTest
 class CourseRepositoryTest
 {
-@Autowired private CourseRepository codeRepository;
+@Autowired private CourseRepository courseRepository;
 
-@Test
+//@Test
 public void saveCourseWithTeacher() {
     Teacher teacher = Teacher.builder()
             .firstName("Priyanka")
@@ -31,14 +35,79 @@ public void saveCourseWithTeacher() {
             .teacher(teacher)
             .build();
 
-    codeRepository.save(course);
+    courseRepository.save(course);
 }
 
 @Test
 public void printCourses() {
-    List<Course> courses = codeRepository.findAll();
+    List<Course> courses = courseRepository.findAll();
     
     System.out.println("courses = " + courses);
 }
+@Test
+public void findAllPagination(){
+    Pageable firstPagewithThreeRecords = PageRequest.of(0, 3);
+    Pageable secondPageWithTwoRecords = PageRequest.of(1,2);
+    
+    Page<Course> page = courseRepository.findAll(firstPagewithThreeRecords);
+    
+    List<Course> courses = page.getContent();
+    long totalElements = page.getTotalElements();
+    long totalPages = page.getTotalPages();
+
+    System.out.println("totalPages = " + totalPages);
+    System.out.println("totalElements = " + totalElements);
+    System.out.println("courses = " + courses);
+}
+
+@Test
+public void findAllSorting() {
+    Pageable sortByTitle =
+            PageRequest.of(
+                    0,
+                    2,
+                    Sort.by("title")
+                    );
+    Pageable sortByCreditDesc =
+            PageRequest.of(
+                    0,
+                    2,
+                    Sort.by("credit").descending()
+            );
+
+    Pageable sortByTitleAndCreditDesc =
+            PageRequest.of(
+                    0,
+                    2,
+                    Sort.by("title")
+                    .descending()
+                    .and(Sort.by("credit"))
+            );
+    
+    List<Course> courses
+            = courseRepository.findAll(sortByTitle).getContent();
+
+    System.out.println("courses = " + courses);
+}
+@Test
+public void printfindByTitleContaining() {
+    Pageable firstPageTenRecords =
+            PageRequest.of(0,10);
+
+    List<Course> courses =
+            courseRepository.findByTitleContaining(
+                    "D",
+                    firstPageTenRecords).getContent();
+
+    System.out.println("courses = " + courses);
+/**
+ * Course(courseId=5, title=DBA, credit=5, courseMaterial=null, teacher=Teacher(teacherId=1, firstName=Qutub, lastName=Khan)), 
+ * Course(courseId=9, title=DBA, credit=5, courseMaterial=null, teacher=null), 
+ * Course(courseId=11, title=DBA, credit=5, courseMaterial=null, teacher=null), 
+ * Course(courseId=13, title=DBA, credit=5, courseMaterial=null, teacher=null)]
+
+ */
+}
+
 
 }
